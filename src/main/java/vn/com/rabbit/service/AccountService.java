1e601c359ab0d66a470266fb03a5a0313a79f767
Nguyễn Công Toàn <tn.toannguyen2811@gmail.com>
2021-04-20 11:08:02 +0700

    fix db

--- src/main/java/vn/com/rabbit/service/AccountService.java
+++ src/main/java/vn/com/rabbit/service/AccountService.java
@@ -1,12 +1,12 @@
 package vn.com.rabbit.service;
 
-import org.springframework.stereotype.Service;
-import vn.com.rabbit.entity.Account;
-import vn.com.rabbit.service.model.ModelBase;
-
-import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import vn.com.rabbit.entity.Account;
+import vn.com.rabbit.service.model.ModelBase;
 
 public interface AccountService {
 
